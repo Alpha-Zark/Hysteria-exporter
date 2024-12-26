edd9352576fbcd81dc7c627197af35ee7b8ef4ac
Zark Lin <zark.lin@onfinality.io>
2024-12-26 18:29:49 +0800

    fix bug

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"hysteria_exporter/web"
+	http_metric "hysteria_exporter/web/metric"
 	"log"
 )
 
 func main() {
 	// Start the Prometheus exporter server on port 9090
-	go web.StartExporterServer(":9090")
+	go http_metric.StartExporterServer(":9090")
 	// Log message to confirm the server is running
 	log.Println("Prometheus exporter is running at http://localhost:9090/metrics")
 	// Blocking main thread (keep server running)
--- web/metric/exporter.go
+++ web/metric/exporter.go
@@ -1,4 +1,4 @@
-package web
+package metric
 
 import (
 	"fmt"
